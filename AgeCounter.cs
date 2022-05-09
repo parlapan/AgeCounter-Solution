56cf5314724e63c91fa905542a5bd413678420c2
parlapan <77237912+parlapan@users.noreply.github.com>
2022-05-09 15:03:39 +0300

    Add files via upload

---
+++ AgeCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AgeCounter
+{
+    public static class AgeCounter
+    {
+        public static int Age(int[] arr)
+        {
+            int age = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                age += arr[i];
+            return age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AgeCounter
+{
+    public static class Program
+    {
+        static void Main(string[] arr)
+        {
+            int age = AgeCounter.Age(new int[] { 5, -2, 3 });
+            if (age <= 0)
+            {
+                Console.WriteLine("The ages not may be negative number");
+            }
+            else if (age >= 1 & age < 18)
+            {
+                Console.WriteLine("The person is not adult");
+            }
+            else if (age > 18 & age < 99)
+            { 
+                Console.WriteLine("The person is adult");
+            }
+            else if (age > 100)
+            {
+                Console.WriteLine("The person is adult but it's to old");
+            }
+        }
+    }
+}
